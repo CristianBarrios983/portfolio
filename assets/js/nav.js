1c7ab9e40099cd14476015c99217919cc24e2572
CristianBarrios983 <barrioscristian.9908@gmail.com>
2023-12-18 17:15:08 -0300

    preloader agregado y pequeña modificacion en el menu

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,3 +3,21 @@ window.addEventListener("scroll", () => {
     const viewport = window.innerHeight;
     nav.classList.toggle("fixed", window.scrollY > viewport);
 });
+
+
+const navbarToggler = document.getElementById('navbarToggler');
+
+navbarToggler.addEventListener("click", () => {
+    // Obtiene el icono del span
+    const icon = navbarToggler.querySelector('i');
+
+    //Cambiar el icono en base al estado del menu
+    if(navbarToggler.classList.contains('collapsed')){
+        //Menu colapsado
+        icon.classList.remove('fa-sharp', 'fa-solid', 'fa-x');
+        icon.classList.add('fa-sharp', 'fa-solid', 'fa-bars');
+    }else{
+        icon.classList.remove('fa-sharp', 'fa-solid', 'fa-bars');
+        icon.classList.add('fa-sharp', 'fa-solid', 'fa-x');
+    }
+})
