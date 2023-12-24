f4a93960384bd1628032d9891051e5d8fe239a13
CristianBarrios983 <barrioscristian.9908@gmail.com>
2023-12-24 20:10:10 -0300

    modal de informacion de proyecto

--- assets/js/proyectos.js
+++ assets/js/proyectos.js
@@ -17,8 +17,8 @@ const cargarProyectos = () => {
                             <h5 class="card-title">${proyecto["nombre-proyecto"]}</h5>
                             <div class="overlay">
                                 <div class="buttons d-flex gap-2">
-                                    ${proyecto['url-codigo'] ? `<a href="${proyecto['url-codigo']}" class="btn btn-primary rounded-0 fs-5" target="_blank">Codigo</a>` : ''}
                                     ${proyecto['url-ver'] ? `<a href="${proyecto['url-ver']}" class="btn btn-danger rounded-0 fs-5" target="_blank">Ver proyecto</a>` : ''}
+                                    <button class="btn btn-info rounded-0 fs-5" onclick="cargarModal('${proyecto["nombre-proyecto"]}', '${proyecto["descripcion"]}', '${proyecto["url-codigo"]}', '${proyecto["lenguajes"]}')" >Info</button>
                                 </div>
                             </div>
                         </div>
@@ -33,3 +33,51 @@ const cargarProyectos = () => {
 }
 
 cargarProyectos();
+
+
+const cargarModal = (nombreProyecto, descripcionProyecto, urlCodigo, lenguajes) => {
+    const modal = document.getElementById('modal');
+    const modalTitle = document.getElementById('projectTitle');
+    const description = document.getElementById('projectDescription');
+    const urlCode = document.getElementById('url-code');
+    const badgeModal = document.getElementById('badges');
+
+
+    badgeModal.innerHTML = '';
+
+    if(nombreProyecto){
+
+        modalTitle.innerHTML = nombreProyecto;
+    }
+
+    if(descripcionProyecto){
+        description.innerHTML = descripcionProyecto;
+    }else{
+        description.innerHTML = "No hay descripcion para este proyecto";
+    }
+
+    if(lenguajes){
+
+        lenguajes = lenguajes.split(',');
+    
+        lenguajes.forEach((lenguaje) => {
+            const span = document.createElement('span');
+            span.className = 'badge text-bg-primary me-1';
+            span.textContent = lenguaje;
+            badgeModal.appendChild(span);
+        });
+    }
+
+    if(urlCodigo){
+        urlCode.href = urlCodigo;
+    }
+
+    modal.style.display = 'block';
+
+}
+
+const cerrarModal = () => {
+    const modal = document.getElementById('modal');
+    
+    modal.style.display = 'none';
+}
