a16ece06245813801fc406a842fdfa6d17e8651d
CristianBarrios983 <barrioscristian.9908@gmail.com>
2023-12-30 18:31:13 -0300

    localStorage en el modo oscuro y box-shadow a servicios

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -8,11 +8,24 @@ document.addEventListener('DOMContentLoaded', function () {
         body.setAttribute('data-bs-theme', 'dark');
         icon.classList.remove('fa-solid','fa-moon');
         icon.classList.add('fa-solid','fa-sun'); // Cambia la clase del icono al modo oscuro
+        // Guarda el modo oscuro en el localStorage
+        localStorage.setItem('mode', 'dark');
       } else {
         body.setAttribute('data-bs-theme', 'light');
         icon.classList.remove('fa-solid','fa-sun');
         icon.classList.add('fa-solid','fa-moon'); // Cambia la clase del icono al modo claro
+        // Guarda el modo claro en el localStorage
+        localStorage.setItem('mode', 'light');
       }
     });
-  });
+
+    if(localStorage.getItem('mode') === 'dark'){
+      darkModeToggle.checked = true;
+      body.setAttribute('data-bs-theme', 'dark');
+      icon.classList.add('fa-solid','fa-sun');
+    }else{
+      body.setAttribute('data-bs-theme', 'light');
+      icon.classList.add('fa-solid','fa-moon');
+    }
+});
   
